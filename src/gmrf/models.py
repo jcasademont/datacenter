3f1aba43d0349fb40531ee1906c05a0561a922f2
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-04-07 17:32:53 +0200

    Remove bayesian model

--- src/gmrf/models.py
+++ src/gmrf/models.py
@@ -1,46 +1,7 @@
-# import pymc as pm
 import numpy as np
 import metrics as m
 from numpy.linalg import inv
 from sklearn.covariance import GraphLassoCV, GraphLasso
-
-
-def bayesian(X):
-    gamma = pm.Gamma('gamma', 10e-6, 10e-6)
-    gamma.value = 1.0
-    alpha = pm.InverseGamma('alpha', 1., gamma / 2)
-
-    N = 0
-    n = 1
-    for i in range(X.shape[1]):
-        N += n
-        n += 1
-
-    Q = inv(np.cov(X))
-    print(Q)
-    jv = Q[np.triu_indices(X.shape[1])]
-
-    j = np.empty(N, dtype=object)
-    for i in range(N):
-        j[i] = pm.Normal("j" + str(i), 0, 1. / alpha, value=jv[i])
-
-    @pm.deterministic
-    def J(j=j):
-        J = np.zeros((X.shape[1], X.shape[1]))
-        inds_u = np.triu_indices(X.shape[1])
-        inds_l = np.tril_indices(X.shape[1])
-        J[inds_u] = j
-        J[inds_l] = j
-        return inv(np.mat(J))
-
-    xs = np.empty(X.shape[0], dtype=object)
-    for i in range(X.shape[0]):
-        xs[i] = pm.MvNormal("x" + str(i), np.zeros(X.shape[1]), J, value=X[i, :].T, observed=True)
-
-    model = pm.Model([gamma, alpha, pm.Container(j), J, pm.Container(xs)])
-    mcmc = pm.MCMC(model)
-    mcmc.sample(10000, 5000)
-
 
 def empirical(S, labels, graph):
     Q = inv(S)
