11279b652ecb7f1e86177d83eaa0c3b709ffd789
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-04-19 23:55:04 +0200

    Fix gmrf unit tests

--- src/gmrf/tests/test_glasso.py
+++ src/gmrf/tests/test_glasso.py
@@ -15,7 +15,13 @@ class TestGlasso(unittest.TestCase):
                                  [0.3, 2.0, 1.0],
                                  [0.0, 1.0, 1.8]])
 
+        self.Q_loop = np.array([[3.0, 0.3, 1.7, 2.0],
+                                [0.3, 2.0, 1.0, 2.3],
+                                [1.7, 1.0, 1.8, 0.4],
+                                [2.0, 2.3, 0.4, 1.3]])
+
         self.chain_data = self.generate_data(self.Q_chain)
+        self.loop_data = self.generate_data(self.Q_loop)
 
     def test_simple_chain_cv(self):
         """ Test glasso with simple model A - B - C using CV """
@@ -23,8 +29,22 @@ class TestGlasso(unittest.TestCase):
         for idx, x in np.ndenumerate(self.Q_chain):
             self.assertLess(abs(Q_pred[idx] - x), 0.2)
 
+    def test_loop_cv(self):
+        """ Test glasso with simple model A - B using CV
+                                          |   |
+                                          C - D
+                                                        """
+        Q_pred, _ = mo.graphlasso(self.loop_data, method='cv')
+        print(Q_pred)
+        for idx, x in np.ndenumerate(self.Q_loop):
+            self.assertLess(abs(Q_pred[idx] - x), 0.2)
+
     def test_simple_chain_bic(self):
         """ Test glasso with simple model A - B - C using BIC """
         Q_pred, _ = mo.graphlasso(self.chain_data, method='bic')
         for idx, x in np.ndenumerate(self.Q_chain):
-            self.assertLess(abs(Q_pred[idx] - x), 0.2)
+            print(abs( x - Q_pred[idx]))
+            if x == 0:
+                self.assertEqual(Q_pred[idx], x)
+            else:
+                self.assertLess(abs(Q_pred[idx] - x), 0.2)
--- src/gmrf/tests/test_metrics.py
+++ src/gmrf/tests/test_metrics.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-import metrics
+import metrics as m
 from numpy.linalg import inv
 from scipy.stats import norm, multivariate_normal
 
@@ -11,7 +11,7 @@ class TestLogPdf(unittest.TestCase):
         x = np.array([1])
         mean = np.array([2])
         Q  = np.array([[ 1 / 25 ]])
-        self.assertAlmostEqual(metrics.logpdf(x, mean, Q),
+        self.assertAlmostEqual(m.logpdf(x, mean, Q),
                                norm.logpdf(1, 2, 5))
 
     def test_mulit_dim(self):
@@ -22,7 +22,7 @@ class TestLogPdf(unittest.TestCase):
                        [0.7, 0.68, 0.01],
                        [-0.4, 0.01, 1]])
 
-        self.assertAlmostEqual(metrics.logpdf(x, mean, Q),
+        self.assertAlmostEqual(m.logpdf(x, mean, Q),
                                multivariate_normal.logpdf(x, mean, inv(Q)))
 
 class TestBic(unittest.TestCase):
@@ -30,11 +30,15 @@ class TestBic(unittest.TestCase):
     def test_simple_chain(self):
         """ Test BIC for simple chain A - B - C"""
         Q = np.array([[1, -0.5, 0], [-0.5, 1.25, -0.5], [0, -0.5, 1]])
-        mean = np.array([0.7, 3, 0.4])
-        X = np.array([[0.2, 1.5, 0.8],
-                      [4.6, 1.3, -0.7],
-                      [8.5, -0.2, 1.7],
-                      [2.7, 0.1, 3.6],
-                      [4.1, -5, 1.7]])
 
-        self.assertAlmostEqual(metrics.bic(X, Q, mean), -170.025628)
+        X = np.array([[-0.5, -1.5, 0.4],
+                      [3.9, -1.7, -1.1],
+                      [7.8, -3.2, 1.3],
+                      [2.0, -2.9, 3.2],
+                      [3.4, -8, 1.3]])
+
+        mean = np.mean(X, axis=0)
+
+        self.assertAlmostEqual(m.bic(X, Q),
+                               np.sum(multivariate_normal.logpdf(X, mean, inv(Q))) - 2.5 * np.log(5))
+
