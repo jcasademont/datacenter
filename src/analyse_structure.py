7a25fe0ed306d020f7b3ee21f959498f3f7f1bb8
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-04-19 23:49:20 +0200

    Use only 'all' graph in analyse

--- src/analyse_structure.py
+++ src/analyse_structure.py
@@ -10,14 +10,16 @@ import matplotlib.pyplot as plt
 def main():
     K = list(layouts.datacenter_layout.keys())
 
-    df = utils.prep_dataframe(keep=K, drop=['outlet', 'room'])
-    Q_rack, alpha_rack = mo.graphlasso(df.values, method="bic")
+    # df = utils.prep_dataframe(keep=K, drop=['outlet', 'room'])
+    # Q_rack, alpha_rack = mo.graphlasso(df.values, method="bic")
 
-    df = utils.prep_dataframe(keep=K, drop=['room'])
-    Q_ahu, alpha_ahu = mo.graphlasso(df.values, method="bic")
+    # df = utils.prep_dataframe(keep=K, drop=['room'])
+    # Q_ahu, alpha_ahu = mo.graphlasso(df.values, method="bic")
 
-    df = utils.prep_dataframe(keep=K)
-    Q_all, alpha_all = mo.graphlasso(df.values, method="bic")
+    df = utils.prep_dataframe(keep=K, remove_mean=True)
+    Q_all, alpha_all, scores = mo.graphlasso(df.values, method="bic", log=True)
+
+    print(alpha_all)
 
     fig, ax = plt.subplots(1, 1)
     G = gr.fromQ(Q_all, df.columns.values)
@@ -25,6 +27,9 @@ def main():
 
     fig2, ax2 = plt.subplots(1, 1)
     pl.bin_precision_matrix(Q_all, df.columns.values, ax2)
+
+    fig3, ax3 = plt.subplots(1, 1)
+    ax3.plot(scores)
 
     plt.show()
 
