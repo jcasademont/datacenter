546a495143f96e37ba054341b20871eac38345b6
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-05-05 01:06:54 +0100

    Break down layouts

--- src/layouts.py
+++ src/layouts.py
@@ -1,9 +1,8 @@
 max_x = 40
 max_y = 40
-datacenter_layout = \
-    {'ahu_1_outlet': (5, 12), 'ahu_2_outlet': (11, 12),
-     'ahu_3_outlet': (5, 0), 'ahu_4_outlet': (11, 0),
-     'eo6': (6, 10), 'e9': (9, 10), 'e12': (12, 10), 'e15': (15, 10),
+
+racks_layout = \
+    {'eo6': (6, 10), 'e9': (9, 10), 'e12': (12, 10), 'e15': (15, 10),
      'e20': (20, 10),
      'h3': (3, 8), 'h5': (5, 8), 'h9': (9, 8), 'h12': (12, 8),
      'h15': (15, 8), 'h19': (19, 8), 'h22': (22, 8), 'h25': (25, 8),
@@ -14,5 +13,13 @@ datacenter_layout = \
      'n14': (14, 4), 'n19': (19, 4), 'n22': (22, 4), 'n25': (25, 4),
      'q1': (1, 2), 'q4a': (4, 2), 'q8': (8, 2), 'q11': (11, 2),
      'q13': (13, 2), 'q20': (20, 2), 'q20': (20, 2), 'q23': (23, 2),
-     'q25': (25, 2),
-     'room_it_power_(kw)': (30, 30)}
+     'q25': (25, 2)}
+
+datacenter_layout = racks_layout.copy()
+datacenter_layout.update(
+    {'ahu_1_outlet': (5, 12), 'ahu_2_outlet': (11, 12),
+     'ahu_3_outlet': (5, 0), 'ahu_4_outlet': (11, 0)})
+
+full_layout = datacenter_layout.copy()
+full_layout.update(
+    {'room_it_power_(kw)': (30, 30)})
