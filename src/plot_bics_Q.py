70d4716f07d47f7b698a50a0610d9d793a7b9f23
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-05-13 15:17:38 +0100

    Generalise plot bic / Q

--- src/plot_bics_Q.py
+++ src/plot_bics_Q.py
@@ -1,36 +1,38 @@
+import argparse
 import numpy as np
 import layouts
 import plot as pl
 import utils
 import matplotlib.pyplot as plt
 
-def main():
+def main(layout, file_q, file_bics):
+    variables = getattr(layouts, layout)
 
-    Q = np.load("non_gauss_precision.npy")
-    s = np.load("non_gauss_bic_score.npy")
-    sg = np.load("gauss_bic_score.npy")
+    K = list(variables.keys())
 
-    K = list(layouts.datacenter_layout.keys())
     df = utils.prep_dataframe(keep=K)
-
     df_shifted = utils.create_shifted_features(df)
-
     df = df.join(df_shifted, how="outer")
-
     df = df.dropna()
 
-    plt.figure()
-    plt.plot(np.arange(0.1, 5, 0.1), s, label="Non gaussian")
-    plt.plot(np.arange(0.1, 5, 0.1), sg, label="Gaussian")
-    plt.legend()
+    Q = np.load(file_q)
+    s = np.load(file_bics)
 
-    # plt.figure()
-    # plt.boxplot(G)
+    plt.figure()
+    plt.plot(np.arange(0.1, 5, 0.1), s)
+
     fig, ax = plt.subplots(1, 1)
     pl.bin_precision_matrix(Q, df.columns.values, ax)
 
     plt.show()
 
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Plot Q and BICs.')
+    parser.add_argument('layout', metavar='LAYOUT',
+                        help="Layout.")
+    parser.add_argument('file_q', metavar='FILE_Q',
+                        help="File containing the prec matrix.")
+    parser.add_argument('file_bics', metavar='FILE_BICS',
+                        help="File containing bics score.")
+    args = parser.parse_args()
+    main(**vars(args))
