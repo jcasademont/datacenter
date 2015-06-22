2ec3ed551ee2ca222f8df3eedcdc9c0b238e845f
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-06-22 08:00:23 +0100

    Update transf

--- src/transformations.py
+++ src/transformations.py
@@ -4,17 +4,20 @@ from scipy.stats import norm
 class CDF_Estimator:
 
     def __init__(self, X):
+        self.n = len(X)
         self.mean = np.mean(X)
         self.std = np.std(X)
+
+        X = (X - self.mean) / self.std
         self.sort_array = np.sort(X)
         self.values = np.unique(self.sort_array)
         self.cdf = self._cdf(self.sort_array)
 
-    def _cdf(self, X):
+    def _cdf(self, sorted_X):
         cdf = np.zeros(len(self.values))
 
         j = 0
-        for v in X:
+        for v in sorted_X:
 
             if v <= self.values[j]:
                 cdf[j] += 1
@@ -22,17 +25,19 @@ class CDF_Estimator:
                 j += 1
                 cdf[j] = cdf[j - 1] + 1
 
-        return cdf / len(X)
+        return cdf / self.n
 
     def get(self, t, delta=None):
+        t = (t - self.mean) / self.std
         idx = np.where(self.values == t)[0][0]
         cdf = self.cdf[idx]
 
-        if delta:
-            if cdf < delta:
-                cdf = delta
-            elif cdf > 1 - delta:
-                cdf = 1 - delta
+        # if delta:
+        #     if cdf < delta:
+        #         cdf = delta
+        #     elif cdf > 1 - delta:
+        #         cdf = 1 - delta
+        cdf = (self.n * cdf) / (self.n + 1)
 
         return self.mean + self.std * norm.ppf(cdf)
 
@@ -41,10 +46,10 @@ def to_normal(X):
     n = Y.shape[0]
 
     estimators = np.empty(shape=Y.shape[1], dtype=object)
+    delta = 1 / (4 * n ** (0.25) * np.sqrt(np.pi * np.log(n)))
     for i in range(Y.shape[1]):
         estimators[i] = CDF_Estimator(Y[:, i])
 
-    delta = 1 / (4 * n ** (0.25) * np.sqrt(np.pi * np.log(n)))
     for (i, j), x in np.ndenumerate(Y):
         Y[i, j] = estimators[j].get(x, delta=delta)
 
