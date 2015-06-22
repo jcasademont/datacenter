a9c9bcb177c347fd4c219ab3b85ba1c37076f17a
Jean Casademont <jean.casademont11@imperial.ac.uk>
2015-06-22 08:04:48 +0100

    Reinformcement learning

--- src/mdp/mdp.py
+++ src/mdp/mdp.py
@@ -1,12 +1,13 @@
 import operator
 import itertools
 import numpy as np
+import multiprocessing as mp
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import train_test_split
 
 class MDP():
     def __init__(self, model, nb_samples, reward, gamma, feature_extractor,
-                 controller, control_vars):
+                 controller, control_vars, n_jobs):
 
         self.model = model
         self.nb_samples = nb_samples
@@ -18,13 +19,18 @@ class MDP():
 
         self.control_vars = control_vars
 
+        self.n_jobs = n_jobs
+
         self.prev_state_indices = []
         self.next_state_indices = []
-        self.controls_indices = []
+        self.prev_controls_indices = []
+        self.next_controls_indices = []
 
         for i, n in enumerate(model.variables_names):
-                if n in control_vars or n[3:] in control_vars:
-                    self.controls_indices.append(i)
+                if n[3:] in control_vars:
+                    self.prev_controls_indices.append(i)
+                elif n in control_vars:
+                    self.next_controls_indices.append(i)
                 elif 'l1_' in n:
                     self.prev_state_indices.append(i)
                 else:
@@ -37,14 +43,30 @@ class MDP():
         outlet_values = []
         for i, n in enumerate(self.control_vars):
             outlet_values.append(controller.get_value(n, controls[i], x))
-        x[0, self.controls_indices] = outlet_values
+        x[0, self.prev_controls_indices] = outlet_values
 
-        p = self.model.predict(x, self.model.variables_names[self.next_state_indices])
+        p = self.model.predict(x, self.model.variables_names[np.append(self.next_state_indices, self.next_controls_indices)])
 
-        return p
+        return p[:, :len(self.next_state_indices)]
 
     def value_function(self, s):
-        return self.linreg.predict(s)[0]
+        x = self.feature_extractor(s)
+        return self.linreg.predict(x)[0]
+
+    def _estimate_value_function(self, s):
+        q = dict()
+        for c in itertools.product(self.controller.values,
+                                   repeat=np.size(self.control_vars)):
+
+            p = self._get_next_state(s, c, self.controller)
+
+            state = dict(zip(self.model.variables_names[self.prev_state_indices], s))
+            action = dict(zip(self.control_vars, c))
+
+            q[c] = self.reward(state, action, self.controller) \
+                 + self.gamma * self.value_function(p)
+
+        return max(q.values())
 
     def learn(self):
         samples = self.model.sample(self.nb_samples)[:, self.prev_state_indices]
@@ -52,27 +74,15 @@ class MDP():
         converged = False
         nb_iter = 0
 
-        y = np.empty(samples.shape[0])
+        X = self.feature_extractor(samples)
+        print("[MDP LEARNING] Linear regression")
+        self.linreg.fit(X, [0] * X.shape[0], n_jobs=4)
 
-        self.linreg.coef_ = np.zeros(np.size(self.next_state_indices))
-        self.linreg.intercept_ = 0
+        pool = mp.Pool(processes=self.n_jobs)
 
-        while not converged:
+        while not converged and nb_iter < 100:
 
-            q = dict()
-            for i, s in enumerate(samples):
-                for c in itertools.product(self.controller.values,
-                                           repeat=np.size(self.control_vars)):
-
-                    p = self._get_next_state(s, c, self.controller)
-
-                    state = dict(zip(self.model.variables_names[self.prev_state_indices], s))
-                    action = dict(zip(self.control_vars, c))
-
-                    q[c] = self.reward(state, action, self.controller) \
-                         + self.gamma * self.value_function(p)
-
-                y[i] = max(q.values())
+            y = pool.map(self._estimate_value_function, samples)
 
             X = self.feature_extractor(samples)
 
--- src/reinflearning.py
+++ src/reinflearning.py
@@ -3,7 +3,10 @@ import itertools
 import layouts
 import argparse
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+import transformations as tr
 
 from mdp.mdp import MDP
 from gaussian.gmrf import GMRF
@@ -11,7 +14,8 @@ from gaussian.hrf import HRF
 from discretiser import Discretiser
 from sklearn.cross_validation import train_test_split
 
-indice_ahu3_air_on = 91
+indice_air_on = {'ahu_1_outlet': 87, 'ahu_2_outlet': 89,
+                 'ahu_3_outlet': 91, 'ahu_4_outlet': 93}
 inter_low = -4.57
 coef_low = 0.32
 
@@ -36,35 +40,79 @@ class Controller():
             c = coef_high
             inter = inter_high
 
-        return state[0, indice_ahu3_air_on] - c * value[1] - inter
+        return state[0, indice_air_on[name]] - c * value[1] - inter
 
 def reward(state, action, controller):
     r = 0
+    gamma = 0.2
+    roi = ['h6', 'eo6', 'e20', 'q4a', 'q13', 'n2', 'q1']
 
     # if np.any(state >= 32):
     #     r = -100 * np.size(np.where(state >= 32)[0])
     racks_values = []
     for k, v in state.items():
-        if 'ahu' not in k:
+        # if 'ahu' not in k and 'air' not in k:
+        if k in roi:
             racks_values.append(v)
-    r = -np.sum(0.2 * np.power(racks_values, 2))
+
+    r_racks = np.sum(0.2 * np.power(racks_values, 2))
+    # r_racks = np.sum(racks_values)
+
+    # max_racks = np.sum(0.2 * np.power([40] * len(racks_values), 2))
 
     controls_cost = []
     for k, v in action.items():
         controls_cost.append(controller.get_cost(k, v, state))
 
-    r -= np.sum(controls_cost)
+    r_power = np.sum(controls_cost)
 
+    r = r_racks + r_power
+
+    # max_power = np.sum([30] * len(controls_cost))
+
+    # r = ((1 - gamma) * r_power + gamma * r_racks) / ((1 - gamma) * max_power + gamma * max_racks)
     # for a in action:
     #     r += np.where(discretiser.values == a)[0][0] \
     #             - np.size(discretiser.values)
 
-    return r
+    return -r
 
 def feature_creator(X):
-    return X
+    # ['h6', 'eo6', 'e20', 'q4a', 'q13', 'n2', 'q1']
+    return X[:, [0, 3, 6, 31, 32, 35, 24, 47]]
 
-def main(layout, model, output):
+def run_simulation(X_test, controls_vars, mdp, model, controller):
+    l1_indices = [i for i, n in enumerate(model.variables_names)
+                    if 'l1_' in n and n[3:] not in controls_vars]
+    names = [n for n in model.variables_names
+                if 'l1_' not in n and n not in controls_vars]
+    controls_indices = [i for i, n in enumerate(model.variables_names)
+                            if n[3:] in controls_vars]
+
+    states = list()
+    actions = list()
+
+    s = X_test[0, l1_indices]
+    for i in range(X_test.shape[0]):
+        a = mdp.get_action(s)
+
+        actions.append(a)
+        states.append(s)
+
+        x = np.zeros((1, len(model.variables_names)))
+        x[0, l1_indices] = s
+
+        outlet_values = []
+        for i, n in enumerate(controls_vars):
+            outlet_values.append(controller.get_value(n, a[i], x))
+
+        x[0, controls_indices] = outlet_values
+
+        s = model.predict(x, names)[0]
+
+    return np.array(actions), np.array(states)
+
+def main(layout, model, transform, output):
     variables = getattr(layouts, layout)
 
     K = list(variables.keys())
@@ -75,6 +123,14 @@ def main(layout, model, output):
     df = df.join(df_shifted, how="outer")
     df = df.dropna()
 
+    # print(list(enumerate(df.columns.values)))
+    # assert False
+
+    if transform:
+        print("* Tranform data")
+        X = tr.to_normal(df.values)
+        df = pd.DataFrame(X, index=df.index.values, columns=df.columns.values)
+
     X = df.values
 
     if model[0] == 'gmrf':
@@ -82,14 +138,18 @@ def main(layout, model, output):
     elif model[0] == 'hybrid':
         model = HRF(k=5, k_star=10, variables_names=df.columns.values)
 
-    train, test = train_test_split(X, test_size=0.25)
+    lim = int(X.shape[0] * 0.75)
+    X_train = X[:lim]
+    X_test = X[lim:]
 
-    model.fit(train)
+    model.fit(X_train)
+    print("* Model Fitted")
 
+    # controls_vars = ['ahu_1_outlet', 'ahu_2_outlet', 'ahu_3_outlet', 'ahu_4_outlet']
     controls_vars = ['ahu_3_outlet']
     controller = Controller(6, 15, 30)
     mdp = MDP(model, 1000, reward, 0.8, feature_creator, controller,
-              controls_vars)
+              controls_vars, n_jobs=3)
     mdp.learn()
 
     # plt.figure()
@@ -98,21 +158,36 @@ def main(layout, model, output):
     # plt.figure()
     # plt.plot(test[:, 38:42])
 
-    l1_indices = [i for i, n in enumerate(model.variables_names) if 'l1_' in n and n[3:] not in controls_vars]
-    actions = list()
-    for i in range(test.shape[0]):
-        actions.append(mdp.get_action(test[i, l1_indices]))
+    actions, states = run_simulation(X_test, controls_vars, mdp, model, controller)
 
-    actions_values_one = [a[0][1] for a in actions if a[0][0] == 0]
-    actions_values_two = [a[0][1] for a in actions if a[0][0] == 1]
-    print(np.sum(actions_values_one + actions_values_two))
-    print(np.mean(np.append(actions_values_one, actions_values_two)))
+    print(actions)
+    actions_values_one = [None] * len(controls_vars)
+    actions_values_two = [None] * len(controls_vars)
+    for i in range(len(controls_vars)):
+        actions_values_one[i] = [(j, a[i][1]) for j, a in enumerate(actions)
+                                if a[i][0] == 0]
+        actions_values_two[i] = [(j, a[i][1]) for j, a in enumerate(actions)
+                                if a[i][0] == 1]
+
+        actions_values_one[i] = list(zip(*actions_values_one[i]))
+        actions_values_two[i] = list(zip(*actions_values_two[i]))
+
+    for i in range(len(controls_vars)):
+        plt.figure()
+        if len(actions_values_one[i]) != 0:
+            plt.plot(list(actions_values_one[i][0]), list(actions_values_one[i][1]), 'b')
+        if len(actions_values_two[i]) != 0:
+            plt.plot(list(actions_values_two[i][0]), list(actions_values_two[i][1]), 'g')
+        plt.title(controls_vars[i])
+
+    max_states = np.amax(states, axis=1)
+    mean_states = np.mean(states, axis=1)
+    min_states = np.amin(states, axis=1)
 
     plt.figure()
-    plt.plot(actions_values_one)
-
-    plt.figure()
-    plt.plot(actions_values_two)
+    plt.plot(max_states, 'r')
+    plt.plot(mean_states, 'g')
+    plt.plot(min_states, 'b')
 
     # plt.figure()
     # plt.plot(actions[:, 0], label="1")
@@ -133,6 +208,9 @@ if __name__ == "__main__":
                         nargs=1, choices=['gmrf', 'hybrid'],
                         default=['gmrf'],
                         help="Model to use.")
+    parser.add_argument('-t', '--transform',
+                        action='store_true', default=False,
+                        help="Transform the data to Gaussian.")
     parser.add_argument('-o', '--output',
                         help="File name to store output data.")
     args = parser.parse_args()
